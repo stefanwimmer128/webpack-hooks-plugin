64b225849933ec71c0124fd8218fad1e6883d519
Stefan Wimmer <info@stefanwimmer128.eu>
2019-11-11 11:25:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+class WebpackHooksPlugin {
+    constructor(hooks) {
+        this.hooks = hooks;
+    }
+    
+    apply(compiler) {
+        Object.keys(this.hooks).forEach(key =>
+            compiler.hooks[key].tap("WebpackHooksPlugin_" + key, this.hooks[key]);
+        });
+    }
+}
